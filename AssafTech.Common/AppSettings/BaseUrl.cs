309cd167c6eb0680899aa0003383cd8c628bee29
Mohammad Assaf <mhmd.assaf24@gmail.com>
2023-06-03 16:25:53 +0300

    fea(baseUrl): update model

--- AssafTech.Common/AppSettings/BaseUrl.cs
+++ AssafTech.Common/AppSettings/BaseUrl.cs
@@ -5,4 +5,6 @@ public class BaseUrl
     public required string Identity { get; set; }
 	public required string Gateway { get; set; }
 	public required string Datalist { get; set; }
+	public required string Mobile { get; set; }
+	public required string Web { get; set; }
 }
