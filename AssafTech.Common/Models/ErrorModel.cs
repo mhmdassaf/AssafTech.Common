0fa34e024abf40f49475ddb4e7f596db35d08a38
Mohammad Assaf <mhmd.assaf24@gmail.com>
2023-06-17 00:25:37 +0300

    feat: add responseModel

---
+++ AssafTech.Common/Models/ErrorModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AssafTech.Common.Models;
+
+public class ErrorModel
+{
+    public required string Code { get; set; }
+    public required string Message { get; set; }
+}
---
+++ AssafTech.Common/Models/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace AssafTech.Common.Models;
+
+public class ResponseModel
+{
+    public bool Succeeded => Errors == null || !Errors.Any();
+    public HttpStatusCode HttpStatusCode { get; set; }
+    public ErrorModel[]? Errors { get; set; }
+    public object? Result { get; set; }
+}
