462671613272861b11268142ec8429b7c34e3050
Mohammad Assaf <mhmd.assaf24@gmail.com>
2023-07-02 11:35:37 +0300

    feat(services): baseService update

--- AssafTech.Common/Services/BaseService.cs
+++ AssafTech.Common/Services/BaseService.cs
@@ -4,10 +4,12 @@ public abstract class BaseService
 {
 	public IRepository _repository { get; }
 	public IMapper _mapper { get; }
+	public HttpContext? _httpContext { get; }
 
-	public BaseService(IRepository repository, IMapper mapper)
+	public BaseService(IRepository repository, IMapper mapper, IHttpContextAccessor httpContextAccessor)
     {
 		_repository = repository;
 		_mapper = mapper;
+		_httpContext = httpContextAccessor.HttpContext;
 	}
 }
